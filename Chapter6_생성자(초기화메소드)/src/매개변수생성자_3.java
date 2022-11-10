8d9b1b1be90c4dad538bd212b8504dd643ff8238
user <user@DESKTOP-COJ9858>
2022-11-10 14:29:37 +0900

    1111

--- Chapter6_생성자(초기화메소드)/src/매개변수생성자_3.java
+++ Chapter6_생성자(초기화메소드)/src/매개변수생성자_3.java
@@ -8,7 +8,7 @@ class Movie
 	{
 		
 	}
-	// Spring, Window
+	// Spring, Window에서 많이 나오는데 우리는 window 안 띄워서 잘 안 씀
 	Movie(Movie m) 
 	{//  --------- 객체를 본 떠온 것
 		System.out.println("mno:"+m.mno);
@@ -26,7 +26,7 @@ public class 매개변수생성자_3 {
 		m1.title = "홍길동전";
 		m1.genre = "액션";
 		
-		Movie m2 = new Movie(m1); // 기존에 있는 거 복사해서 사용하기 298page
+		Movie m2 = new Movie(m1); // 생성자를 이용한 인스턴스 복사 : 기존에 있는 거 복사해서 사용하기 298page
 		
 	}
 
